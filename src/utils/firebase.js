f645554a818bee8e848416f18aa5eb5f54379849
Tim Coutinho <tmcoutinho42@gmail.com>
2020-03-04 11:41:41 -0800

    Remove secrets from version control

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,13 +1,24 @@
 import { initializeApp, database } from "firebase";
 
+import {
+    apiKey,
+    authDomain,
+    databaseURL,
+    projectId,
+    storageBucket,
+    messagingSenderId,
+    appId
+} from "./secrets.js";
+
+
 const firebaseConfig = {
-    apiKey: "AIzaSyD7rWhYuPqTD57YN3m5eOl_Ajo-V3OTw4E",
-    authDomain: "recipes-74822.firebaseapp.com",
-    databaseURL: "https://recipes-74822.firebaseio.com",
-    projectId: "recipes-74822",
-    storageBucket: "recipes-74822.appspot.com",
-    messagingSenderId: "625938140579",
-    appId: "1:625938140579:web:1b7a1009c9d1e793c41e16"
+    apiKey: apiKey,
+    authDomain: authDomain,
+    databaseURL: databaseURL,
+    projectId: projectId,
+    storageBucket: storageBucket,
+    messagingSenderId: messagingSenderId,
+    appId: appId
 };
 initializeApp(firebaseConfig);
 export default database();
