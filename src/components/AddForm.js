23a317f86d1f3990272acd1a0fa6f2d6cd281bf4
Tim Coutinho <tmcoutinho42@gmail.com>
2020-03-12 21:15:01 -0700

    Change AddForm back to functional component

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 
 import "./AddForm.scss";
 
@@ -12,99 +12,87 @@ const initialForm = {
     "desc": ""
 };
 
-export default class AddForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            form: {...initialForm, ...this.props.initialValues},
-            submitHover: false
-        };
-    }
+export default function AddForm(props) {
+    const [submitHover, setSubmitHover] = useState(false);
+    const [form, setForm] = useState({...initialForm, ...props.initialValues});
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        const {initialValues, visible} = this.props;
-        if (visible !== prevProps.visible || initialValues !== prevProps.initialValues) {
-            this.setState({form: {...initialForm, ...initialValues}});
-        }
-    }
+    useEffect(() => {
+        setForm({...initialForm, ...props.initialValues});
+    }, [props.visible, props.initialValues]);
 
-    handleFormChange = e => {
+    const handleFormChange = e => {
         e.preventDefault();
         const {name, value} = e.target;
-        this.setState({form: {...this.state.form, [name]: value}});
+        setForm({...form, [name]: value});
     };
 
-    handleSubmit = e => {
+    const handleSubmit = e => {
         e.preventDefault();
-        if (this.valid()[0]) {
+        if (valid()[0]) {
             return;
         }
-        const {form} = this.state;
         const lastEditedTime = new Date().getTime();
-        this.props.handleAddRecipe({
+        props.handleAddRecipe({
             ...form,
             originalSubmitTime: form.lastEditedTime ? form.lastEditedTime : lastEditedTime,
             lastEditedTime
         });
     };
 
-    valid = () => {
+    const valid = () => {
         const errors = {
-            name: this.state.form.name.length === 0
+            name: form.name.length === 0
         };
         return [Object.keys(errors).some(x => errors[x]), errors];
     };
 
-    render() {
-        const {form} = this.state;
-        const [invalid, errors] = this.valid();
+    const [invalid, errors] = valid();
 
-        return (
-            <div id="add-form-card" className={`${this.props.visible ? "visible" : ""} card`}>
-                <form id="add-form" onSubmit={this.handleSubmit}>
-                    <input
-                        type="text"
-                        className={this.state.submitHover && errors["name"] ? "error" : ""}
-                        onChange={this.handleFormChange}
-                        name="name"
-                        value={form.name}
-                        placeholder="Recipe Name"
-                    />
-                    <br/>
-                    <input
-                        type="text"
-                        className={this.state.submitHover && errors["desc"] ? "error" : ""}
-                        onChange={this.handleFormChange}
-                        name="desc"
-                        value={form.desc}
-                        placeholder="Description"
-                    />
-                    <br/>
-                    <input
-                        type="text"
-                        className={this.state.submitHover && errors["imgSrc"] ? "error" : ""}
-                        onChange={this.handleFormChange}
-                        name="imgSrc"
-                        value={form.imgSrc}
-                        placeholder="Image URL"
-                    />
-                    <div
-                        id="add-form-cancel"
-                        className="form-btn primary-btn"
-                        onClick={() => this.props.handleAddRecipe()}
-                    >
-                        Cancel
-                    </div>
-                    <input
-                        type="submit"
-                        id="add-form-submit"
-                        className={`${invalid ? "error" : ""} form-btn primary-btn`}
-                        value="Save Recipe"
-                        onMouseEnter={() => this.setState({submitHover: true})}
-                        onMouseLeave={() => this.setState({submitHover: false})}
-                    />
-                </form>
-            </div>
-        );
-    }
+    return (
+        <div id="add-form-card" className={`${props.visible ? "visible" : ""} card`}>
+            <form id="add-form" onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    className={submitHover && errors["name"] ? "error" : ""}
+                    onChange={handleFormChange}
+                    name="name"
+                    value={form.name}
+                    placeholder="Recipe Name"
+                />
+                <br/>
+                <input
+                    type="text"
+                    className={submitHover && errors["desc"] ? "error" : ""}
+                    onChange={handleFormChange}
+                    name="desc"
+                    value={form.desc}
+                    placeholder="Description"
+                />
+                <br/>
+                <input
+                    type="text"
+                    className={submitHover && errors["imgSrc"] ? "error" : ""}
+                    onChange={handleFormChange}
+                    name="imgSrc"
+                    value={form.imgSrc}
+                    placeholder="Image URL"
+                />
+                <div
+                    id="add-form-cancel"
+                    className="form-btn primary-btn"
+                    onClick={() => props.handleAddRecipe()}
+                >
+                    Cancel
+                </div>
+                <input
+                    type="submit"
+                    id="add-form-submit"
+                    className={`${invalid ? "error" : ""} form-btn primary-btn`}
+                    value="Save Recipe"
+                    onMouseEnter={() => setSubmitHover(true)}
+                    onMouseLeave={() => setSubmitHover(false)}
+                />
+            </form>
+        </div>
+    );
 }
